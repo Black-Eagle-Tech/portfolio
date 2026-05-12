267ce76016461b98831ffb51eac0add1e903dd36
Mauro Vicens <blackeagledhs@gmail.com>
2026-05-12 10:41:26 -0300

    change in vite

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
-import ghPages from 'gh-pages'
 
 export default defineConfig({
   plugins: [vue()],
+
   base: '/portfolio/',
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
